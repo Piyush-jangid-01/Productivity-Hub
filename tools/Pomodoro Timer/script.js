affe2ef04b16cac3ed7553b61ed1188f48799492
Piyush <jangidpiyush16011@gmail.com>
2026-01-20 23:04:49 +0530

    Add files via upload

---
+++ tools/Pomodoro Timer/script.js
@@ -0,0 +1,13 @@
+let worktime = 25;
+let breaktime = 5;
+let longbreaktime = 15;
+let currenttime = worktime * 60;
+let totaltime = currenttime;
+let isRunning = false;
+let isWorkSession = true;
+let seasoncount = 0;
+let completedseasons = 0;
+let totalminutes = 0;
+let currentstreak = 0;
+let timer = null;
+
